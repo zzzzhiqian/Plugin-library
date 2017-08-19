3dc12e7501f339200e039456b4c00be8b257e32d
protosl <644074553@qq.com>
2017-08-19 12:07:24 -0700

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <button type="button" name="button" class="btn btn-default">btn-default</button>
+    <button type="button" name="button" class="btn btn-info">btn-info</button>
+    <button type="button" name="button" class="btn btn-warning">btn-warning</button>
+    <button type="button" name="button" class="btn btn-danger">btn-danger</button>
+    <button type="button" name="button" class="btn btn-success">btn-success</button>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
